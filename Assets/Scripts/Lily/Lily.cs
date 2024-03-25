84923f60ac71734ed16187113076b07ba1955b89
zhanghangyu <15605213849@163.com>
2024-03-26 00:11:38 +0800

    fix bug for Lily move

--- Assets/Scripts/Lily/Lily.cs
+++ Assets/Scripts/Lily/Lily.cs
@@ -9,7 +9,7 @@ public class Lily : MonoBehaviour
 {
     public float mSpeed = 5.0f;  //Lily移动速度
 
-    private float mAngle = 0.0f; //Lily面朝角度
+    private bool mFaceToward = true;  //Lily朝向 —— true为右，false为左
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +20,22 @@ public class Lily : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 faceToward = Vector2.zero;
-        faceToward.x = Input.GetAxis("Vertical");
-        faceToward.y = Input.GetAxis("Horizontal");
-
-        float angle = Mathf.Atan2(faceToward.y,faceToward.x) * Mathf.Rad2Deg;
-        mAngle = faceToward.x == 0 && faceToward.y == 0 ? mAngle : angle;
+        Vector2 moveVec = Vector2.zero;
+        moveVec.y = Input.GetAxis("Vertical");
+        moveVec.x = Input.GetAxis("Horizontal");
+        if(moveVec.x > 0)
+        {
+            mFaceToward = true;
+        }
+        else if(moveVec.x < 0)
+        {
+            mFaceToward = false;
+        }
         
-        transform.localRotation = Quaternion.Euler(0, 0, -mAngle);
+        GetComponent<SpriteRenderer>().flipX = !mFaceToward;
 
-        if (faceToward.x != 0 || faceToward.y != 0)
-            transform.Translate(transform.up * mSpeed * Time.smoothDeltaTime, Space.World);
+        transform.Translate(moveVec.y * Vector3.up * mSpeed * Time.smoothDeltaTime, Space.World);
+        transform.Translate(moveVec.x * Vector3.right * mSpeed * Time.smoothDeltaTime, Space.World);
     }
 
 }
