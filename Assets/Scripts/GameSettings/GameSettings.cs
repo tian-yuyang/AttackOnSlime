9ddc12130e5f2d5f788552b74495964ce71c9504
zhanghangyu <15605213849@163.com>
2024-03-26 17:12:49 +0800

    new interval logic

--- Assets/Scripts/GameSettings/GameSettings.cs
+++ Assets/Scripts/GameSettings/GameSettings.cs
@@ -1,30 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameSettings : MonoBehaviour
 {
-    [Tooltip("Tail Node间隔(单位为frame)")]
-    public int TailNodeInterval = 3;
-
-    [Tooltip("First Tail Node相对于起始位置的偏移")]
-    public int FirstTailNodeOffset = 2;
-
     // Start is called before the first frame update
     private void Awake()
     {
         Application.targetFrameRate = 30; //锁定三十帧
-        TailNodeBehavior.FirstSearchPosOffset = FirstTailNodeOffset;
-        TailNodeBehavior.SearchInterval = TailNodeInterval;
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
