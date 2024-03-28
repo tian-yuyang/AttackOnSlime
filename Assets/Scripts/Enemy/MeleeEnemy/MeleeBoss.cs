c6c830fed0c4a673adc80e57c826d54080be015a
hjm <2237649923@qq.com>
2024-03-28 18:04:11 +0800

    “restore”

---
+++ Assets/Scripts/Enemy/MeleeEnemy/MeleeBoss.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeBoss : Enemy
+{
+    public float rushSpeed = 30f;
+    private int counter = 0;
+    private const int STOP = 0;
+    private const int MOVE = 1;
+    private const int RUSH = 2;
+    private Vector3 direction;
+    private float distance = 0f;
+    private int status = MOVE;
+
+    public CoolDownBar coolDown = null;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Init();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        // be rushing to Lily
+        if (status == RUSH)
+        {
+            Rush();
+            return;
+        }
+
+        if (status == STOP)
+        {
+            return;
+        }
+
+        // alert mode
+        if (GetTargetDistance() < alertDistance)
+        {
+            Attack();
+            if (coolDown.ReadyForNext())
+            {
+                if (counter % 4 == 0)
+                {
+                    AdvanceAttack();
+                }
+                counter = (counter + 1) % 60;
+                coolDown.TriggerCoolDown();
+            }
+
+        }
+    }
+
+    void Init()
+    {
+        status = MOVE;
+        SetSpeed(5f);
+        SetLife(50);
+        SetAttack(1);
+    }
+
+    void Attack()
+    {
+        Vector3 p = transform.position;
+        p += speed * Time.smoothDeltaTime * GetTargetDirection();
+        transform.position = p;
+    }
+
+    void AdvanceAttack()
+    {
+        direction = GetTargetDirection();
+        distance = GetTargetDistance();
+
+        GameObject rectangle = new GameObject("Rectangle");
+        rectangle.AddComponent<SpriteRenderer>().color = Color.red;
+
+        Vector3 scale = new Vector3(distance, 1f, 1f);
+        rectangle.transform.localScale = scale;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rectangle.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Vector3 position = transform.position + direction * (distance / 2f);
+        rectangle.transform.position = position;
+
+        status = STOP;
+        Invoke("ChangeToRush", 0.8f);
+    }
+
+    void ChangeToRush()
+    {
+        status = RUSH;
+    }
+
+    void Rush()
+    {
+        transform.position = rushSpeed * Time.smoothDeltaTime * direction;
+        distance -= rushSpeed * Time.smoothDeltaTime;
+        if (distance < 0)
+        {
+            status = MOVE;
+        }
+    }
+
+    protected override void OnCollisionStay2D(Collision2D objectName)
+    {
+        base.OnCollisionStay2D(objectName);
+
+        if (objectName.gameObject.tag == "Player")
+        {
+            targetHero.Damage(GetAttack());
+        }
+    }
+}
