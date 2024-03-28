31cda29ae13d72d9cbfa12308040ecb2294c452c
Qianyu Liu <liuqianyu420@gmail.com>
2024-03-28 15:41:20 +0800

    add feature: heal when killing

---
+++ Assets/Scripts/Enemy/HealStone.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealStone : MonoBehaviour
+{
+    private int heal = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Kill()
+    {
+        Destroy(transform.gameObject);
+    }
+    
+    void OnTriggerEnter2D(Collider2D objectName)
+    {
+        if (objectName.gameObject.tag == "Player")
+        {
+            targetHero.Damage(-heal);
+            Kill();
+        }
+    }
+}
